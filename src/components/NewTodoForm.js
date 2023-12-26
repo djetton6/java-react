853e35e88b14793b674cbdfdb91c9ea590e0bb6f
Desmend Jetton <desmendjetton@gmail.com>
2023-12-26 01:36:51 -0800

    adding state to a form section

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,16 +1,34 @@
 import { useState, React } from "react";
 
 function  NewTodoForm() {
+    const [assigned, setAssigned] = useState('');
+    const [description, setDescription] = useState('');
+
+
+    const assignedChange = (event) => {
+        console.log('assigned' ,event.target.value)
+        setAssigned(event.target.value);
+    }
+
+    const descriptionChange = (event) => {
+        console.log('description', event.target.value);
+        setDescription(event.target.value);
+    }
     return(
         <div className="mt-5">
             <form>
                 <div className="mb-3">
                     <label className="form-label">Assigned</label>
-                    <input type="text" className="form-control" required></input>
+                    <input type="text" className="form-control" required
+                    onChange={assignedChange}></input>
+                    value={assigned}
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Description</label>
-                    <input type="text" className="form-control" rows={3} required></input>
+                    <input type="text" className="form-control" rows={3} required
+                    onChange={descriptionChange}
+                    value={description}
+                    ></input>
                 </div>
                 <button type="button" className="btn btn-primary mt-3">Add Todo</button>
             </form>
